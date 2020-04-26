ad2f7f99aff69c777d2a6a704136c31d357e4fca
Shijun  Yu <yus3@rose-hulman.edu>
2020-04-26 00:46:41 -0400

    Updates

---
+++ precache-manifest.0a2c8b172ca35ec4d5a9e39b09dbfe12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "505442a619f5004080b5b939ea7a0569",
+    "url": "/sundial/index.html"
+  },
+  {
+    "revision": "1c4d370650e2b63edfc4",
+    "url": "/sundial/static/css/main.aa8ba17e.chunk.css"
+  },
+  {
+    "revision": "f4616385320526131b96",
+    "url": "/sundial/static/js/2.ff0bf491.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/sundial/static/js/2.ff0bf491.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c4d370650e2b63edfc4",
+    "url": "/sundial/static/js/main.09838fca.chunk.js"
+  },
+  {
+    "revision": "2a7052a305d00c6462bc",
+    "url": "/sundial/static/js/runtime-main.6cb49573.js"
+  }
+]);
